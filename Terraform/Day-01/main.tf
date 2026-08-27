31cf97fa4f581737903ecff9ca6c648a53645a25
Gaurang Rastogi <gaurangrastogi09@gmail.com>
2026-08-27 18:24:26 +0000

    Add Terraform Day 01 fundamentals

---
+++ Terraform/Day-01/main.tf
@@ -0,0 +1,19 @@
+terraform{
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+}
+}
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-learning"
+  }
+}
